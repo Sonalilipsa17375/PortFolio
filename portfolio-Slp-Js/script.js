74a788e517d1fab39a67387f1fb98c58d4b86263
Sonali lipsa Patra <138273956+Sonalilipsa17375@users.noreply.github.com>
2024-07-28 23:39:46 +0530

    Update script.js

--- portfolio-Slp-Js/script.js
+++ portfolio-Slp-Js/script.js
@@ -1,25 +1,37 @@
-let menuIcon = document.querySelector('#menu');
+let menu = document.querySelector('#menu');
+
 let navbar = document.querySelector('.nav');
 
-menuIcon.onclick = () => {
+menu.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
 
 let sections = document.querySelectorAll('section');
+
 let navlinks = document.querySelectorAll('header nav a');
 
-window.onscroll =() => {
-    sections.forEach(sec =>{
-        let top = window.scrollY;
-        let offset= sec.offsetTop -100;
+window.onscroll = () => {
+    
+    let top = window.scrollY;
+    
+    sections.forEach(sec => {
+        
+        let offset = sec.offsetTop - 100;
+        
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
-         
-        if(top>= offset && top < offset + height){
-            navlinks.classList.remove('active');
-            document.querySelector('header nav a[href*='+ id + ']').classList.add('active');
-        };
-    })
+        
+        if (top >= offset && top < offset + height) {
+            
+            navlinks.forEach(link => {
+                link.classList.remove('active');
+            });
+            
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        }
+    });
+
     let header = document.querySelector(".header");
+    
 }
